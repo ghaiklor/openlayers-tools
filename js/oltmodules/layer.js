6bc0078dc0bb17704b352991c075376e50735874
ghaiklor <ghaiklor@gmail.com>
2013-03-21 15:07:39 +0200

    Update layer Class. Added functions addMap and addVectorLayer. Realized children->parent connection in OpenLayersTools.

--- js/oltmodules/layer.js
+++ js/oltmodules/layer.js
@@ -51,16 +51,16 @@ Layer.prototype = {
                     layers.push(new OpenLayers.Layer.OSM());
                     break;
                 default:
-                    this.parent.Console.writeWarning('Wrong layer type! Please check your parameters in function addMap!');
+                    this.parent.Console.writeWarning('Func: addMap | Wrong layer type! Please check your parameters in function addMap!');
                     break;
             }
         }
         if (layers.length != 0) {
             this.parent.Map.addLayers(layers);
-            this.parent.Console.writeDebug('Layers is not null and it\'s added to OpenLayersTools.Map!');
+            this.parent.Console.writeDebug('Func: addMap | Layers is not null and it\'s added to OpenLayersTools.Map!');
             return true;
         } else {
-            this.parent.Console.writeError('The count of layers is 0! Check parameters for function addMap!');
+            this.parent.Console.writeError('Func: addMap | The count of layers is 0! Check parameters for function addMap!');
             return false;
         }
     },
@@ -69,7 +69,7 @@ Layer.prototype = {
 
         function makeStyleAndPushToArray(renderIntent) {
             if (['default', 'select', 'temporary'].indexOf(renderIntent) === -1) {
-                parent.Console.writeWarning('Wrong render intent for style! Check parameters!');
+                parent.Console.writeWarning('Func: addVectorLayer | Wrong render intent for style! Check parameters!');
                 return false;
             }
             stylesMap[renderIntent] = new OpenLayers.Style(config.styleMap[renderIntent]);
@@ -77,19 +77,73 @@ Layer.prototype = {
         }
 
         if (config.styleMap == undefined) {
-            this.parent.Console.writeInfo('StyleMap in config of addVectorLayer is empty! Set rules for StyleMap');
+            this.parent.Console.writeInfo('Func: addVectorLayer | StyleMap in config of addVectorLayer is empty! Set rules for StyleMap');
             return false;
         }
 
         var stylesMap = [];
         for (var style in config.styleMap) {
             if (!makeStyleAndPushToArray(style)) {
-                this.parent.Console.writeWarning('Error in makeStyleMap! Check addVectorLayer parameters!');
+                this.parent.Console.writeWarning('Func: addVectorLayer | Error in makeStyleMap! Check addVectorLayer parameters!');
             }
         }
         stylesMap = new OpenLayers.StyleMap(stylesMap);
         config.styleMap = stylesMap;
         this.parent.Map.addLayer(new OpenLayers.Layer.Vector(name, config));
         return true;
+    },
+    getLayerByName: function (name) {
+        var layer = this.parent.Map.getLayersByName(name)[0];
+        if (layer == undefined) {
+            this.parent.Console.writeError('Func: getLayerByName | Layer ' + name + ' doesn\'t exists!');
+            return false;
+        }
+        return layer;
+    },
+    getLayersByName: function (name) {
+        var layers = this.parent.Map.getLayersByName(name);
+        if (layers == undefined) {
+            this.parent.Console.writeError('Func: getLayersByName | Layer ' + name + ' doesn\'t exists!');
+            return false;
+        }
+        return layers;
+    },
+    getFeatureById: function (layerName, id) {
+        var layer = this.getLayerByName(layerName);
+        if (layer == undefined) {
+            this.parent.Console.writeError('Func: getFeatureById | Layer ' + name + ' doesn\'t exists!');
+            return false;
+        }
+        if (layer.features == undefined) {
+            this.parent.Console.writeError('Func: getFeatureById | Layer ' + name + ' don\'t have features!');
+            return false;
+        }
+        for (var feature in layer.features) {
+            if (layer.features[feature] != undefined) {
+                if (layer.features[feature].attributes.id == id) {
+                    return layer.features[feature];
+                }
+            }
+        }
+    },
+    getFeaturesById: function (layerName, id) {
+        var layer = this.getLayerByName(layerName);
+        var features = [];
+        if (layer == undefined) {
+            this.parent.Console.writeError('Func: getFeaturesById | Layer ' + name + ' doesn\'t exists!');
+            return false;
+        }
+        if (layer.features == undefined) {
+            this.parent.Console.writeError('Func: getFeaturesById | Layer ' + name + ' don\'t have features!');
+            return false;
+        }
+        for (var feature in layer.features) {
+            if (layer.features[feature] != undefined) {
+                if (layer.features[feature].attributes.id == id) {
+                    features.push(layer.features[feature]);
+                }
+            }
+        }
+        return features;
     }
 };
